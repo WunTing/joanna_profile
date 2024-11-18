eaa8759f56d2e02c623bfe23e8fd3d114a356bb1
Joanna <46317766+WunTing@users.noreply.github.com>
2024-11-18 19:01:55 +0800

    Add files via upload

---
+++ src/JS/side-menu.js
@@ -0,0 +1,55 @@
+// pin側選單，滾動監聽
+// $(document).ready(function () {
+//     var sectionIds = $('.pin-menu').find('.pin-menu-input').find('a.input');
+
+//     $(document).scroll(function () {
+//         sectionIds.each(function () {
+
+//             var container = $(this).attr('href');
+//             var containerOffset = $(container).offset().top;
+//             var containerHeight = $(container).outerHeight();
+//             var containerBottom = containerOffset + containerHeight;
+//             var scrollPosition = $(document).scrollTop();
+
+//             if (scrollPosition < containerBottom - 160 && scrollPosition >= containerOffset - 160) {
+//                 $(this).addClass('active');
+//             } else {
+//                 $(this).removeClass('active');
+//             }
+
+
+//         });
+//     });
+// });
+$(document).ready(function () {
+    var sectionIds = $('.pin-menu').find('.pin-menu-input').find('a.input');
+
+    // 點擊事件處理，讓選單點擊後對應段落捲動到頁面頂端
+    sectionIds.on('click', function (e) {
+        e.preventDefault(); // 避免預設行為
+
+        var targetSection = $($(this).attr('href')); // 獲取目標段落
+        var offset = $(window).width() <= 767 ? 100 : 80; // 在767以下改為100
+        $('html, body').animate({
+            scrollTop: targetSection.offset().top - offset // 調整為使段落位於頂部，並加入 offset
+        }, 500); // 設置滾動時間
+    });
+
+    $(document).scroll(function () {
+        sectionIds.each(function () {
+            var container = $(this).attr('href');
+            var containerOffset = $(container).offset().top;
+            var containerHeight = $(container).outerHeight();
+            var containerBottom = containerOffset + containerHeight;
+            var scrollPosition = $(document).scrollTop();
+
+            var offset = $(window).width() <= 767 ? 110 : 100; // 在767以下改為110
+
+            if (scrollPosition < containerBottom - offset && scrollPosition >= containerOffset - offset) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
+});
---
+++ src/JS/slick.js
@@ -0,0 +1,29 @@
+// slick輪播
+$(document).ready(function () {
+    $('.slider-item').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        initialSlide: 1, // 從第三張幻燈片開始（索引從 0 開始，所以 1 表示第二張）
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button type="button" class="custom-prev-arrow"></button>',//自定義箭頭
+        nextArrow: '<button type="button" class="custom-next-arrow"></button>',//自定義箭頭
+        responsive: [
+            {
+                breakpoint: 1024, // RWD在1024寬度時切換顯示數量
+                settings: {
+                    slidesToShow: 2, //一次顯示3個
+                    slidesToScroll: 2,//切換下一頁時移動3個
+                }
+            }, {
+                breakpoint: 600,// RWD在600寬度時切換顯示數量
+                settings: {
+                    slidesToShow: 1,//一次顯示2個
+                    slidesToScroll: 1,//切換下一頁時移動2個
+                    arrows: false,
+                }
+            },
+        ]
+    });
+});
